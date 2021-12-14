798f5272d75022a3e8e565908b229129ee825044
Zenilton Sousa <ze18.fv@gmail.com>
2021-12-14 19:36:04 -0300

    validaçao do form

--- processa_envio.php
+++ processa_envio.php
@@ -1,5 +1,42 @@
 <?php
 
-print_r($_POST) ;
+    class Mensagem{
+
+        private $para= null;
+        private $assunto=null;
+        private $mensagem=null;
+
+
+        function __get($attr){
+            return $this->$attr;
+        }
+
+        function __set($attr,$value){
+            $this->$attr=$value;
+        }
+
+        function MensagemValida(){
+            if(empty($this->para)||empty($this->assunto)|| empty($this->mensagem)){
+               return false;
+            }else{
+                return true;
+            }
+
+        }
+
+    }
+
+    $mensagem= new Mensagem();
+    $mensagem->__set('para', $_POST['para']);
+    $mensagem->__set('assunto', $_POST['assunto']);
+    $mensagem->__set('mensagem', $_POST['mensagem']);
+
+    if($mensagem->MensagemValida()){
+            echo'mensagem valida';
+    }else{
+        echo'nao valida';
+    }
+
+
 
 ?>
